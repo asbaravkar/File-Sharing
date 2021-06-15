4ad9fc001048ca06916c40e17a32499f12bc025d
Aditya <aditya.baravkar@gmail.com>
2021-06-15 11:49:13 +0530

    Creating schema and MongoDB connection

--- server/models/File.ts
+++ server/models/File.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose'
+import mongoose, {Document} from 'mongoose'
 
 const Schema = mongoose.Schema
 
@@ -6,5 +6,36 @@ const fileSchema = new Schema({
     filename : {
         type : String,
         required : true
+    },
+    secure_url : {
+        type : String,
+        required : true
+    },
+    format : {
+        type : String,
+        required : true
+    },
+    sizeInBytes : {
+        type : String,
+        required : true
+    },
+    sender : {
+        type : String,
+    },
+    receiver : {
+        type : String,
     }
+},{
+    timestamps : true
 })
+
+interface IFile extends Document {
+    filename : string,
+    secure_url : string,
+    sizeInBytes : string,
+    format : string,
+    sender? : string,
+    receiver? : string,
+}
+
+export default mongoose.model<IFile>("File", fileSchema)
